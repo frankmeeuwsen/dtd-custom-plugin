5b0f95bf0dcaa8fdfb88b04cdc68f6a53b062f27
Frank Meeuwsen <frank@incredibleadventure.nl>
2022-06-23 08:49:18 +0200

    Add delay in RSS feed

--- dtd-custom-plugin.php
+++ dtd-custom-plugin.php
@@ -653,3 +653,25 @@ function customRSSFunc()
 {
 	get_template_part('feed', 'dtd');
 }
+
+function publish_later_on_feed($where) {
+ 
+    global $wpdb;
+ 
+    if ( is_feed() ) {
+        // timestamp in WP-format
+        $now = gmdate('Y-m-d H:i:s');
+ 
+        // value for wait; + device
+        $wait = '30'; // integer
+ 
+        // http://dev.mysql.com/doc/refman/5.0/en/date-and-time-functions.html#function_timestampdiff
+        $device = 'MINUTE'; //MINUTE, HOUR, DAY, WEEK, MONTH, YEAR
+ 
+        // add SQL-sytax to default $where
+        $where .= " AND TIMESTAMPDIFF($device, $wpdb->posts.post_date_gmt, '$now') > $wait ";
+    }
+    return $where;
+}
+ 
+add_filter('posts_where', 'publish_later_on_feed');
